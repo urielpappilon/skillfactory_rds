e824bbd6c8abb286522eb797e761480d53c52319
Yan Habis <yan@habis.by>
2020-04-28 21:18:29 +0300

    Fixed possible shadowing issues with module name; Fixed multiple redundant parentheses issues; PEP 257:changed docstring single quotes to double quotes

--- module_0/main.py
+++ module_0/main.py
@@ -1,18 +1,18 @@
 import numpy as np
-import game_core
+import core
 
 
 def score_game(game_core):
-    '''Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число'''
+    """Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число"""
     count_ls = []
     np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
-    random_array = np.random.randint(1, 101, size=(1000))
+    random_array = np.random.randint(1, 101, size=1000)
     for number in random_array:
         count_ls.append(game_core(number))
     score = int(np.mean(count_ls))
     print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
-    return (score)
+    return score
 
 
 # running test
-score_game(game_core.game_core_v3)
+score_game(core.game_core_v3)
