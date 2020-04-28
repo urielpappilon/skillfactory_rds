23056f5dd83433e852944156047187bf2bd97631
Yan Habis <yan@habis.by>
2020-04-28 21:06:33 +0300

    Split up game_core functions from the scoring function in main module Added v3 of game_core (mean for finding number is 4 compared to 101 and 33 for v1 and v2 respectively)

---
+++ module_0/game_core.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+
+def game_core_v1(number):
+    '''Просто угадываем на random, никак не используя информацию о больше или меньше.
+       Функция принимает загаданное число и возвращает число попыток'''
+    count = 0
+    while True:
+        count += 1
+        predict = np.random.randint(1, 101)  # предполагаемое число
+        if number == predict:
+            return (count)  # выход из цикла, если угадали
+
+
+def game_core_v2(number):
+    '''Сначала устанавливаем любое random число, а потом уменьшаем или увеличиваем его в зависимости от того, больше оно или меньше нужного.
+       Функция принимает загаданное число и возвращает число попыток'''
+    count = 1
+    predict = np.random.randint(1, 101)
+    while number != predict:
+        count += 1
+        if number > predict:
+            predict += 1
+        elif number < predict:
+            predict -= 1
+    return (count)  # выход из цикла, если угадали
+
+
+def game_core_v3(number):
+    """Binary search core"""
+    count = 0
+    # Given that number is from 1 to 100, so the interval length is 100
+    # we iterate until middle integer is not our number
+    low = 1
+    high = 101
+    lookup_range = 100
+    middle = lookup_range // 2
+    while middle != number:
+        # increasing attempt counter
+        count += 1
+        # since middle is not our number - adjusting boundaries
+        if number > middle:
+            low = middle + 1  # shifting lower boundary to the number is bigger than guess
+        elif number < middle:
+            high = middle - 1  # otherwise shifting higher boundary to the middle
+        middle = (low + high) // 2  # finding new middle after adjusting boundaries
+    return count
+
--- module_0/main.py
+++ module_0/main.py
@@ -1,29 +1,6 @@
 import numpy as np
+import game_core
 
-
-def game_core_v1(number):
-    '''Просто угадываем на random, никак не используя информацию о больше или меньше.
-       Функция принимает загаданное число и возвращает число попыток'''
-    count = 0
-    while True:
-        count += 1
-        predict = np.random.randint(1, 101)  # предполагаемое число
-        if number == predict:
-            return (count)  # выход из цикла, если угадали
-
-
-def game_core_v2(number):
-    '''Сначала устанавливаем любое random число, а потом уменьшаем или увеличиваем его в зависимости от того, больше оно или меньше нужного.
-       Функция принимает загаданное число и возвращает число попыток'''
-    count = 1
-    predict = np.random.randint(1,101)
-    while number != predict:
-        count+=1
-        if number > predict:
-            predict += 1
-        elif number < predict:
-            predict -= 1
-    return(count) # выход из цикла, если угадали
 
 def score_game(game_core):
     '''Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число'''
@@ -37,8 +14,5 @@ def score_game(game_core):
     return (score)
 
 
-# запускаем
-score_game(game_core_v1)
-
-# Проверяем
-score_game(game_core_v2)
+# running test
+score_game(game_core.game_core_v3)
